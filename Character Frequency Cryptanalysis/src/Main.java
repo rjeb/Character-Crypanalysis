1c71e7cecb2aa64b95c3d8f9d8b7ae9b50c529c0
Jason Thai <jasonnthaii@gmail.com>
2018-09-13 19:41:30 -0700

    Basic functionality done

--- Character Frequency Cryptanalysis/src/Main.java
+++ Character Frequency Cryptanalysis/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,9 +10,16 @@ import java.util.TreeMap;
 
 public class Main {
 
+	public static TreeMap<Character, Integer> sortMapByValue(TreeMap<Character, Integer> in) {
+		
+		
+		return in;
+	}
+	
 	public static void main(String [] args) {
 		String ciphertext;
 		String plaintext;
+		ArrayList<Character> cipherKey = new ArrayList<Character>();
 		
 		ArrayList<Character> chars = new ArrayList<Character>(
 				Arrays.asList('a','b','c','d','e','f','g','h',
@@ -28,6 +36,8 @@ public class Main {
 			characterFrequencies.put(chars.get(i), 0);
 		}
 		
+		ArrayList<Map.Entry<Character, Integer>> usedCharacters = new ArrayList<Map.Entry<Character, Integer>>();
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Please enter a ciphertext:");
 		ciphertext = scanner.nextLine().toLowerCase();
@@ -41,10 +51,34 @@ public class Main {
 		System.out.println("Character Frequencies:");
 		for (Map.Entry<Character, Integer> c : characterFrequencies.entrySet()) {
 			if (c.getValue()>0) {
-				System.out.println(c + ": " + c.getValue());
+				usedCharacters.add(c);
 			}
 		}
+		//comparator to sort entries of Map by their value(Integer) in decreasing order
+		Comparator<Map.Entry<Character, Integer>> valueComparator = new Comparator<Map.Entry<Character, Integer>>() {
+		    public int compare(Map.Entry<Character, Integer> entry1, Map.Entry<Character, Integer> entry2) {
+		        int result = entry2.getValue().compareTo(entry1.getValue());
+
+		        if (result == 0) {
+		        	//sorts entries by decreasing value
+		            return entry2.getValue() - entry1.getValue();
+		        }
+		        
+		        return result;
+
+		    }
+		};
+
 		
+		usedCharacters.sort(valueComparator);
+		
+		for (int i = 0; i < usedCharacters.size(); i++) {
+			cipherKey.add(usedCharacters.get(i).getKey());
+		}
+		
+		Cipher textCipher = new Cipher(cipherKey);
+		
+		System.out.println("Plaintext estimate using statistics of most used letters: \n" + textCipher.decrypt(ciphertext));
 		
 		scanner.close();
 	}
